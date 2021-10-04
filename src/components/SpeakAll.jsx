93cf04112621c15998e9a8350f44e2f72d3594a3
Ol_Kuts <kuts.ol.vas@gmail.com>
2021-10-04 14:52:07 +0300

    change homepage

--- src/components/SpeakAll.jsx
+++ src/components/SpeakAll.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {verbs} from "../data/data";
 import st from './styles/LeftPanel.module.css';
 import {VerbColumn} from "./VerbColumn";
